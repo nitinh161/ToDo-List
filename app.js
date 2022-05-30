7069f524e0bcf2d79e5b2903dde5eaa161236ed1
Nitin Hans <nitinh.cs.19@nitj.ac.in>
2022-05-31 01:45:31 +0530

    Stage 1.0

--- app.js
+++ app.js
@@ -3,34 +3,26 @@ const bodyParser = require("body-parser");
 const { listen } = require("express/lib/application");
 
 const app = express();
-
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    switch (currentDay) {
-        case 0: day="Sunday";
-            break;
-        case 1: day="Monday";
-            break;
-        case 2: day="Tuesday";
-            break;
-        case 3: day="Wednesday";
-            break;
-        case 4: day="Thursday";
-            break;
-        case 5: day="Friday";
-            break;
-        case 6: day="Saturday";
-            break;
-        default:
-            console.log("Error: currdate is equal to " + currentDay);
-    }
-    res.render("list", { kindOfDay: day });
+    let today = new Date();
+    let currentDay = today.getDay();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", { kindOfDay: day, newListItems: items });
 });
-
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("server running on port 3000");
