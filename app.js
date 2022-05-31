981f83425881dc089ad063b6f011cb28757cdb2b
Nitin Hans <nitinh.cs.19@nitj.ac.in>
2022-05-31 17:25:37 +0530

    Stage 1.1

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require("body-parser");
 const { listen } = require("express/lib/application");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = []; 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.get("/", function (req, res) {
 
     let today = new Date();
